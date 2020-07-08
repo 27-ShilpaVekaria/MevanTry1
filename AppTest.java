7691d8a0d1cffc61e5a7abc22faaa74f82f4d87a
27-ShilpaVekaria <66478189+27-ShilpaVekaria@users.noreply.github.com>
2020-07-08 23:38:47 +0100

    Add files via upload

---
+++ AppTest.java
@@ -0,0 +1,12 @@
+package org.example;
+public class AppTest 
+{
+//    /**
+//     * Rigorous Test :-)
+//
+//    @Test
+//    public void shouldAnswerWithTrue()
+//    {
+//        assertTrue( true );
+//    }
+}
